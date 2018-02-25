1fcddcecb8c4561ba973150862994c13f8ded715
leehyunggeun <leehyunggeun@navercorp.com>
2018-02-26 00:50:53 +0800

    Bug Fix: call path

--- src/tsToMp4.js
+++ src/tsToMp4.js
@@ -19,7 +19,6 @@ function args() {
     }
     let input_path = argv._[0];  // input file path
     let output_path = argv.output || input_path.replace(/(?:\/)(.)+\.ts/i, '$1.mp4');   // output file path
-    input_path = path.resolve(input_path);
     return {
         input_path,
         output_path
